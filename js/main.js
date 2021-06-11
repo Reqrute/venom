1facf3420614d8e311fcd94a99ad4c1c8722ef67
Roman <85631572+Reqrute@users.noreply.github.com>
2021-06-11 21:39:11 +0300

    final act

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuCloseButton = document.querySelector('.menu-close');
+
+menuButton.addEventListener("click", () =>{
+    menu.classList.add("is-active");
+    menuCloseButton.classList.add("is-active");
+});
+menuCloseButton.addEventListener("click",()=>{
+    menu.classList.remove("is-active");
+    menuCloseButton.classList.remove("is-active");
+})
